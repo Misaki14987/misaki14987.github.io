a9674d4e2b399df6b1d37f7cc76811c8a0ca465b
misaki <2775631440@qq.com>
2026-04-11 04:20:29 +0000

    haha i like nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Misaki's Blog - Astro Site";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+            pnpm
+            git
+          ];
+
+          shellHook = ''
+            echo "🚀 Misaki's Blog dev shell"
+            echo "Node: $(node --version)"
+            echo "pnpm: $(pnpm --version)"
+          '';
+        };
+      });
+}
