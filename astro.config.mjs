bcfeaeb26e3569155e78627d49e38f1f6641728d
misaki <2775631440@qq.com>
2026-08-10 09:27:59 +0800

    feat: publish feedback stability article

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.yodelist.icu',
   integrations: [sitemap()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
